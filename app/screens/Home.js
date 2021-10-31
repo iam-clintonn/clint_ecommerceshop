752b9615552cd92fa700c8cbbc772eec4dfa17fc
iam-clintonn <davidclinton1.dc@gmail.com>
2021-10-31 23:40:18 +0000

    login Page Upgrade

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+
 
 const Home = () => {
     return (
-        <SafeAreaView>
-            <Text> Home Page </Text>
+        <SafeAreaView style={{backgroundColor: '#ADD8E6', flex:1, alignItems:'center'}}>
+            <Text style={{backgroundColor:'#fff', fontSize: 25, padding: 10, margin:30}}> Home Page </Text>
         </SafeAreaView>
     )
 }
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
 const Login = ({navigation}) => {
 
     const handleLogin = () => {
@@ -11,13 +12,16 @@ const Login = ({navigation}) => {
 
     return (
         <SafeAreaView style={{backgroundColor:"#ADD8E6", flex: 1, justifyContent:"center", alignItems:"center"}}>
-            <Text style={{marginBottom:40, fontSize: 20}}>Welcome to Clint's Shop</Text>
-            <TextInput style={{backgroundColor: "#fff", padding: 10, borderRadius: 5, margin: 12, color: "grey", width: "60%"}} placeholder="Username"/>
-            <TextInput style={{backgroundColor: "#fff", padding: 10, borderRadius: 5, margin: 12, color: "grey", width: "60%"}} placeholder="Password" secureTextEntry/>
-            {/* secureTextEntry for hiding passwords when entered into placeholder */}
+            <Text style={{marginBottom: 30, fontSize: 30}}>Welcome to Grocify</Text>
+            <Image source={{uri: 'https://images.unsplash.com/photo-1550989460-0adf9ea622e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'}}
+                style={{width: 150, height: 130, borderRadius: 60}} />
 
+            <TextInput style={{backgroundColor: "#fff", padding: 10, borderRadius: 5, margin: 12, color: "grey", width: "60%"}} placeholder="Enter Username"/>
+            <TextInput style={{backgroundColor: "#fff", padding: 10, borderRadius: 5, margin: 12, color: "grey", width: "60%"}} placeholder="Enter Password" secureTextEntry/>
+            {/* secureTextEntry for hiding passwords characters when entered into placeholder */}
+            
 
-            <TouchableOpacity style={{backgroundColor: "#2e4", width: "60%", justifyContent: "center", alignItems: "center", padding: 10}} onPress = {handleLogin}><Text style={{color: '#fff', fontSize: 18}}>Login</Text></TouchableOpacity>
+            <TouchableOpacity style={{backgroundColor: "#2e6", width: "40%", justifyContent: "center", margin: 9 , alignItems: "center", padding: 9}} onPress = {handleLogin}><Text style={{color: '#fff', fontSize: 18}}>Login</Text></TouchableOpacity>
         </SafeAreaView>
     )
 
