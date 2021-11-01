dfc32a356f5893d7430b1f2208a7ce559ff34d09
iam-clintonn <davidclinton1.dc@gmail.com>
2021-11-01 20:56:36 +0000

    API fetch

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import React, {useEffect, useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import axios from 'axios' 
 
 
 const Home = ({navigation}) => {
 
-const handleHome = () =>{
-    navigation.navigate('ProductDetails');
-}
+    const handleHome = () =>{
+        navigation.navigate('ProductDetails');
+    }
+
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        axios 
+            .get("https://fakestoreapi.com/products")
+            .then((data) => setProducts(data.data))
+            .catch((err) => alert(err.message))
+    }, [])
 
     return (
         <SafeAreaView style={{backgroundColor: '#ADD8E6', flex:1, alignItems:'center'}}>
-            <Text style={{backgroundColor:'#fff', fontSize: 25, padding: 10, margin:30}}> Home Page </Text>
-            <TouchableOpacity style={{backgroundColor:'#fff', fontSize:15, padding:6, margin:20, width: "20%", justifyContent:'center', alignItems: 'center' }}><Text> Fruit </Text></TouchableOpacity>
-            <TouchableOpacity><Text style={{backgroundColor:'#fff', fontSize:15, padding:6, margin:20 }}> Vegetables </Text></TouchableOpacity>
+            {/* {products && <FlatList data={products} renderItem={(product3) => (<Text key={product.id}>{product.title}</Text>)}/>} */}
+            <ScrollView style={{flex: 1}}>
+                {products && products.map(product => <Text key={product.id}>{product.title}</Text>)}
 
+            </ScrollView>
         </SafeAreaView>
     )
 }
