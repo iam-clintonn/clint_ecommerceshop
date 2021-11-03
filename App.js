cb257948b7839b958aab068595eab17d2664ed57
iam-clintonn <davidclinton1.dc@gmail.com>
2021-11-03 21:28:31 +0000

    Home Screen Revised

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRoute="login"
+        initialRoute="Home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={Login} />
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,11 +4,14 @@ import {
   Text,
   View,
   TouchableOpacity,
-  FlatList,
+  TouchableHighlight,
   ScrollView,
+  TextInput,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
+
+import { Octicons, Ionicons } from "@expo/vector-icons";
 import Product from "../components/Product";
 
 const Home = ({ navigation }) => {
@@ -25,22 +28,77 @@ const Home = ({ navigation }) => {
     <SafeAreaView
       style={{ backgroundColor: "#ADD8E6", flex: 1, alignItems: "center" }}
     >
-      <ScrollView style={{ flex: 1 }}>
-        {products &&
-          products.map((product) => (
-            <Product
-              switchPage={() =>
-                navigation.navigate("ProductDetails", { ...product })
-              }
-              title={product.title}
-              image={product.image}
-              price={product.price}
-              key={product.id}
-            />
-          ))}
-      </ScrollView>
+      <View style={styles.firstContainer}>
+        <TouchableOpacity>
+          <Ionicons name="menu-outline" size={24} color="black" />
+        </TouchableOpacity>
+        <TextInput style={styles.input} placeholder="Search Groceries" />
+        <TouchableOpacity>
+          <Octicons name="settings" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.secondContainer}>
+        <Text style={{ fontSize: 30, letterSpacing: 2 }}>Groceries</Text>
+      </View>
+      <View style={styles.thirdContainer}>
+        <TouchableOpacity style={styles.tab}>
+          <Text style={styles.tabText}>Fruits</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tab}>
+          <Text style={styles.tabText}>Vegetables</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tab}>
+          <Text style={styles.tabText}>Others</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView style={styles.scrollViewer}></ScrollView>
+      <View style={styles.bottomContainer}></View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#e8f4f8",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 200,
+    borderRadius: 5,
+  },
+  firstContainer: {
+    flexBasis: "10%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  secondContainer: {
+    flexBasis: "5%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  thirdContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  tab: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#e8f4f8",
+  },
+  tabText: {
+    fontSize: 18,
+  },
+  scrollViewer: {
+    flexBasis: "50%",
+    backgroundColor: "red",
+  },
+  bottomContainer: {
+    flex: 1,
+  },
+});
 
 export default Home;
